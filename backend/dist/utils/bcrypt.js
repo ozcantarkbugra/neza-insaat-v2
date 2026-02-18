4f40d3d52c3446aa04a77351911260419a166421
TarÄ±k BuÄŸra Ã–zcan <ozcantarkbugra@outlook.com>
2026-02-18 03:00:39 +0300

    chore: replace bcrypt with bcryptjs in dependencies and update related imports for improved compatibility

--- backend/dist/utils/bcrypt.js
+++ backend/dist/utils/bcrypt.js
@@ -5,12 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hashPassword = hashPassword;
 exports.comparePassword = comparePassword;
-const bcrypt_1 = __importDefault(require("bcrypt"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const SALT_ROUNDS = 10;
 async function hashPassword(password) {
-    return bcrypt_1.default.hash(password, SALT_ROUNDS);
+    return bcryptjs_1.default.hash(password, SALT_ROUNDS);
 }
 async function comparePassword(password, hashedPassword) {
-    return bcrypt_1.default.compare(password, hashedPassword);
+    return bcryptjs_1.default.compare(password, hashedPassword);
 }
 //# sourceMappingURL=bcrypt.js.map
